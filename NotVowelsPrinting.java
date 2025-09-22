b0967f3215a1d19c0814e7dcc9537d07532bf25b
SriRam S <sri223770@gmail.com>
2025-09-22 13:48:27 +0530

    Only printing Consonents not Vowels

---
+++ NotVowelsPrinting.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+     public class NotVowelsPrinting {
+           public static void main(String[] args) {
+             Scanner sc = new Scanner(System.in);
+             String input = sc.nextLine();
+             String[] arr = input.split("[aeiouAEΙΟΥ]");
+            for (String str: arr) {
+           System.out.print(str);
+            }
+           }
+        }
